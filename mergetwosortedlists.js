d6ce594260a784ff21a1f7cf0448c8e357dc0fba
llleeeaaannn <piercefrank10@gmail.com>
2023-02-12 21:25:37 +0000

    Almost functional solution, simply missing handling for when both lists are empty

--- mergetwosortedlists.js
+++ mergetwosortedlists.js
@@ -10,20 +10,48 @@ function ListNode(val, next) {
 	this.next = (next === undefined ? null : next)
 }
 
+var checkLast = function(node1, node2) {
+		if (node1 === null && node2 === null) return false;
+    return true;
+}
+
 var mergeTwoLists = function(list1, list2) {
     let current = new ListNode();
     const head = current;
-    while (list1.next || list2.next) {
-        if (list1.val >= list2.val) {
+    console.log('Here')
+    while (list1 || list2) {
+    		if (list1 === null) {
+        		console.log(list2.val);
+            current.val = list2.val;
+            list2 = list2.next;
+            if (checkLast(list1, list2)) {
+            	current.next = new ListNode();
+            	current = current.next;
+            }
+        } else if (list2 === null) {
+        		console.log(list1.val);
             current.val = list1.val;
-            current.next = new ListNode();
-            current = current.next;
             list1 = list1.next;
+            if (checkLast(list1, list2)) {
+            	current.next = new ListNode();
+            	current = current.next;
+            }
+        } else if (list1.val <= list2.val) {
+        		console.log(list1.val);
+            current.val = list1.val;
+            list1 = list1.next;
+            if (checkLast(list1, list2)) {
+            	current.next = new ListNode();
+            	current = current.next;
+            }
         } else {
-            current.val = list1.val;
-            current.next = new ListNode();
-            current = current.next;
-            list1 = list1.next;
+        		console.log(list2.val);
+            current.val = list2.val;
+            list2 = list2.next;
+            if (checkLast(list1, list2)) {
+            	current.next = new ListNode();
+            	current = current.next;
+            }
         }
     }
     return head;
