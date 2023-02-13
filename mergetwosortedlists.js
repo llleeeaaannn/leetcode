6028fb7db6fd0d4cb5e139e5cf9b6ffa73368ece
llleeeaaannn <piercefrank10@gmail.com>
2023-02-13 21:14:37 +0000

    Removed comments:

--- mergetwosortedlists.js
+++ mergetwosortedlists.js
@@ -5,8 +5,6 @@
 //Input: list1 = [1,2,4], list2 = [1,3,4]
 //Output: [1,1,2,3,4,4]
 
-// REFACTORRRRRRRR
-
 function ListNode(val, next) {
 	this.val = (val === undefined ? 0 : val)
 	this.next = (next === undefined ? null : next)
@@ -44,5 +42,3 @@ var mergeTwoLists = function(list1, list2) {
     }
     return head;
 };
-
-// REFACTORRRRRRRR
