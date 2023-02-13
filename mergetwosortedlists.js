a433cc28393b21ef7f84046062e99ff9e103710d
llleeeaaannn <piercefrank10@gmail.com>
2023-02-13 21:13:39 +0000

    Refactored to remove repeating logic

--- mergetwosortedlists.js
+++ mergetwosortedlists.js
@@ -17,39 +17,29 @@ var checkLast = function(node1, node2) {
     return true;
 }
 
+if (list1 === null && list2 === null) return;
+
 var mergeTwoLists = function(list1, list2) {
     let current = new ListNode(null);
     const head = current;
     if (!list1 && !list2) return null;
     while (list1 || list2) {
     		if (list1 === null) {
-            current.val = list2.val;
-            list2 = list2.next;
-            if (checkLast(list1, list2)) {
-            	current.next = new ListNode();
-            	current = current.next;
-            }
+          current.val = list2.val;
+          list2 = list2.next;
         } else if (list2 === null) {
             current.val = list1.val;
             list1 = list1.next;
-            if (checkLast(list1, list2)) {
-            	current.next = new ListNode();
-            	current = current.next;
-            }
         } else if (list1.val <= list2.val) {
             current.val = list1.val;
             list1 = list1.next;
-            if (checkLast(list1, list2)) {
-            	current.next = new ListNode();
-            	current = current.next;
-            }
         } else {
             current.val = list2.val;
             list2 = list2.next;
-            if (checkLast(list1, list2)) {
-            	current.next = new ListNode();
-            	current = current.next;
-            }
+        }
+        if (checkLast(list1, list2)) {
+          current.next = new ListNode();
+          current = current.next;
         }
     }
     return head;
