10df7d4546a5db647e7ba57ff110c2d0663d1527
llleeeaaannn <piercefrank10@gmail.com>
2023-02-13 21:37:14 +0000

    Added handling for one param being null

--- mergetwosortedlists.js
+++ mergetwosortedlists.js
@@ -2,8 +2,8 @@
 // Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
 // Return the head of the merged linked list.
 
-//Input: list1 = [1,2,4], list2 = [1,3,4]
-//Output: [1,1,2,3,4,4]
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
 
 function ListNode(val, next) {
 	this.val = (val === undefined ? 0 : val)
@@ -14,19 +14,23 @@ var mergeTwoLists = function(list1, list2) {
     let current = new ListNode(null);
     const head = current;
     if (!list1 && !list2) return null;
+    if (list1 === null) return list2;
+    if (list2 === null) return list1;
     while (list1 || list2) {
     		if (list1 === null) {
           current.val = list2.val;
-          list2 = list2.next;
+          current.next = list2.next;
+          return head;
         } else if (list2 === null) {
-            current.val = list1.val;
-            list1 = list1.next;
+          current.val = list1.val;
+          current.next = list1.next;
+          return head;
         } else if (list1.val <= list2.val) {
-            current.val = list1.val;
-            list1 = list1.next;
+          current.val = list1.val;
+          list1 = list1.next;
         } else {
-            current.val = list2.val;
-            list2 = list2.next;
+          current.val = list2.val;
+          list2 = list2.next;
         }
         if (list1 === null && list2 === null) return head;
         current.next = new ListNode();
