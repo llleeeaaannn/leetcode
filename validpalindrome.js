e7cc1b26b14af8836b3db1e09fbd8790108452ff
llleeeaaannn <piercefrank10@gmail.com>
2023-02-19 11:39:41 +0000

    Created succesful function

--- validpalindrome.js
+++ validpalindrome.js
@@ -9,3 +9,13 @@
 // Input: s = "race a car"
 // Output: false
 // Explanation: "raceacar" is not a palindrome.
+
+var isPalindrome = function(s) {
+  let parsed = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+  let l = parsed.length / 2;
+  if (parsed.length < 2) return true;
+  let start = parsed.slice(0, Math.floor(l));
+  let end = parsed.slice(Math.ceil(l)).split("").reverse().join("");
+  if (start === end) return true;
+  return false;
+};
