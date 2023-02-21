7234ad56693b992b15886472cb71cbe7bd113519
llleeeaaannn <piercefrank10@gmail.com>
2023-02-21 23:46:37 +0000

    Added comments to solution and perf stats

--- climbingstairs.js
+++ climbingstairs.js
@@ -25,14 +25,22 @@
 // 5. 2 steps + 2 steps
 
 var climbStairs = function(n) {
+  // First two pointers store the first two numbers of the Fibonacci sequence
   let prev = 0;
   let cur = 1;
   let temp;
 
   for (let i = 0; i < n; i++) {
+    // Store prev value in temp
+    // Update prev to be current
+    // Then add temp to current
+    // This is because the next number is equal to the sum of the previous two numbers.
     temp = prev;
     prev = cur;
     cur += temp;
   }
   return cur;
 };
+
+// Runtime beats 76.8%
+// Memory beats 9.7%
