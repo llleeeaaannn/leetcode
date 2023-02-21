a7117308e32fc6ba13c96de311660c934e6f2b9e
llleeeaaannn <piercefrank10@gmail.com>
2023-02-21 23:36:01 +0000

    Added extra context

--- climbingstairs.js
+++ climbingstairs.js
@@ -14,3 +14,13 @@
 // 1. 1 step + 1 step + 1 step
 // 2. 1 step + 2 steps
 // 3. 2 steps + 1 step
+
+
+// Input: n = 4
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step + 1 step
+// 2. 2 steps + 1 step + 1 step
+// 3. 1 step + 2 steps + 1 step
+// 4. 1 step + 1 step + 2 steps
+// 5. 2 steps + 2 steps
