4edcc735b05c330ff682bba8ca65a8a37ce28bff
llleeeaaannn <piercefrank10@gmail.com>
2023-03-10 17:08:36 +0000

    Removed inline comments

--- plusone.js
+++ plusone.js
@@ -15,10 +15,10 @@
 // Thus, the result should be [1,0].
 
 var plusOne = function(digits) {
-  const fromArray = digits.join(''); // get Digits out of Array
-  const toInt = BigInt(fromArray) ; //Dont use parseInt, use BigInt instead
-  const addOne = toInt + 1n; // add one!
-  const result =  Array.from(String(addOne), Number); // return digits to array
+  const fromArray = digits.join('');
+  const toInt = BigInt(fromArray) ;
+  const addOne = toInt + 1n;
+  const result =  Array.from(String(addOne), Number);
   return result
 };
 
